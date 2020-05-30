dd24b7d27b2f270340e2177b997a49aae1417c2a
hirohero830 <58774319+hirohero830@users.noreply.github.com>
2020-05-31 00:35:22 +0900

    Q１〜Q７までを回答

--- task.rb
+++ task.rb
@@ -5,6 +5,7 @@ def q1
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
+puts names << "斎藤"
 
 end
 
@@ -14,14 +15,16 @@ def q2
 
   # 以下に回答を記載
  array = array1 + array2
- p array
- 
+ puts array
+
 end
 
 def q3
   numbers = [1, 5, 8, 10, 2, 3, 2, 3, 3, 1, 4, 5, 9]
 
   # 以下に回答を記載
+  puts numbers.count(3)
+
 
 end
 
@@ -29,7 +32,7 @@ def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
-
+ sports.compact!
   # 以下は変更しないで下さい
   p sports
 end
@@ -39,13 +42,16 @@ def q5
   array2 = [1, 5, 8, 10]
 
   # 以下に回答を記載
-
+  p array1.empty?
+  p array2.empty?
 end
 
 def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
+numbers2 = numbers1.map{|n| n * 10}
+puts numbers2
 
 end
 
@@ -53,7 +59,7 @@ def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
-
+  array.map!(&:to_i)
   # 以下は変更しないで下さい
   p array
 end
