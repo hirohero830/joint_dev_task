25136fce3c42ac628aba10279e272e7de05c7fb6
hirohero830 <58774319+hirohero830@users.noreply.github.com>
2020-06-04 23:14:11 +0900

    Q9,10を解答

--- task.rb
+++ task.rb
@@ -72,21 +72,30 @@ def q8
   upper_case_programming_languages = programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
-  p upper_case_programming_languages
+  p upper_case_programming_langu1ages
 end
 
 def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-
+  names.each.with_index(1) do |name,i|
+    puts "会員No.#{i} #{name}さん"
+  end
 end
 
 def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
-
+  foods.each do|food|
+    if
+      food == "うに"
+      puts "好物です"
+    else
+      puts "まぁまぁ好きです"
+    end
+  end  
 end
 
 def q11
