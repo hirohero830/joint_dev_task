1b940f2f8a8ea5119945e9f63bc75b8c8a9c1bf3
hirohero830 <58774319+hirohero830@users.noreply.github.com>
2020-06-10 21:58:36 +0900

    Q15を解答

--- task.rb
+++ task.rb
@@ -138,7 +138,8 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
-
+puts data1.has_key?(:age)? "OK" : "NG"
+puts data2.has_key?(:age)? "OK" : "NG"
 end
 
 def q16
